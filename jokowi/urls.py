b1438764d3f93f9af3e94889223d5de869927d84
fajriefji <fajri@alphatech.id>
2019-02-14 20:26:56 +0700

    tambah Contact

--- jokowi/urls.py
+++ jokowi/urls.py
@@ -5,5 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('contact/', include('contact.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
