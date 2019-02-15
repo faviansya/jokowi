fe36209356089509c672aeec7ab76c9246b02e01
faviansya <faviansya@alphatech.id>
2019-02-15 10:40:12 +0700

    Tambah Fitur Posting

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('beritalengkap', views.beritalengkap, name='beritalengkap'),
+    path('tambahberita', views.tambahberita, name='tambahberita'),
     path('<int:blog_id>', views.lengkap, name='fullpage'),
     path('<int:blog_id>/comment', views.tambahcomment, name='commentpage'),
+    path('waiting', views.WaitingAff, name='waiting'),
+
 ]
